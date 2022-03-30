cf782c8f7fe2af960b251724b2cbd39616168553
pauline-wim <linette.wim@hotmail.com>
2022-03-30 11:43:43 +0200

    Created user model

---
+++ models/userModel.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+const validateEmail = function (email) {
+  const regex = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regex.test(email);
+};
+
+const UserSchema = mongoose.Schema({
+  email: {
+    type: String,
+    trim: true,
+    lowercase: true,
+    unique: true,
+    required: "Email address is required",
+    validate: [validateEmail, "Please fill a valid email address"],
+    match: [
+      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+      "Please fill a valid email address",
+    ],
+  },
+  password: {
+    type: String,
+    minlength: 6,
+  },
+});
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;

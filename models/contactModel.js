7897574a086aa98c53f452919877ea08168254ec
pauline-wim <linette.wim@hotmail.com>
2022-03-30 17:27:54 +0200

    Added timestamps to schema

--- models/contactModel.js
+++ models/contactModel.js
@@ -5,28 +5,32 @@ const validateEmail = function (email) {
   return regex.test(email);
 };
 
-const ContactSchema = mongoose.Schema({
-  userId: { type: mongoose.Types.ObjectId, ref: "User" },
-  name: {
-    type: String,
-    minlength: 1,
-    maxlength: 30,
+const ContactSchema = mongoose.Schema(
+  {
+    userId: { type: mongoose.Types.ObjectId, ref: "User" },
+    name: {
+      type: String,
+      minlength: 1,
+      maxlength: 30,
+    },
+    email: {
+      type: String,
+      trim: true,
+      lowercase: true,
+      required: "Email address is required",
+      validate: [validateEmail, "Please fill a valid email address"],
+      match: [
+        /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+        "Please fill a valid email address",
+      ],
+    },
+    description: String,
+    categorie: Number,
   },
-  email: {
-    type: String,
-    trim: true,
-    lowercase: true,
-    unique: true,
-    required: "Email address is required",
-    validate: [validateEmail, "Please fill a valid email address"],
-    match: [
-      /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
-      "Please fill a valid email address",
-    ],
-  },
-  description: String,
-  categorie: Number,
-});
+  {
+    timestamps: true,
+  }
+);
 
 const Contact = mongoose.model("Contact", ContactSchema);
 

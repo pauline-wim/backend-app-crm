8f4c77f6b8b193fa7c148e7410f7fadf14e66a87
pauline-wim <linette.wim@hotmail.com>
2022-03-30 15:35:38 +0200

    Created token -> route: login

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
-// const jwt = require("jsonwebtoken");
-// const cookieParser = require("cookie-parser");
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const dotenv = require("dotenv");
 const app = express();
 dotenv.config({
   path: "./config.env",
 });
 
+const secret = "QzF9mq4F4GK678bzPba12JD479Gh9YF5Hr4wixJ8LmK28G976d";
+
 // Models
 const User = require("./models/userModel");
+const Contact = require("./models/contactModel");
 
 // Connexion to MongoDB
 mongoose
@@ -21,7 +24,7 @@ mongoose
 
 // Middlewares
 app.use(express.json());
-// app.use(cookieParser());
+app.use(cookieParser());
 
 // ROUTES
 // Home
@@ -54,19 +57,32 @@ app.post("/register", async (req, res) => {
 });
 
 // Connect to personal account
-app.post("/login", (_req, res) => {
-  res.send("Login");
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+  const user = await User.findOne({ email });
+  if (!user) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+  const isPasswordValid = await bcrypt.compare(password, user.password);
+  if (!isPasswordValid) {
+    return res.status(400).json({
+      message: "Invalid email or password",
+    });
+  }
+  const token = jwt.sign({ id: user._id }, secret);
+  res.cookie("jwt", token, { httpOnly: true, secure: false });
+  res.json({
+    message: "Successfully connected",
+  });
 });
 
 // Add contacts
-app.post("/contacts", (_req, res) => {
-  res.send("Add contact");
-});
+app.post("/contacts", validate, async (req, res) => {});
 
 // Get contacts
-app.get("/contacts", (_req, res) => {
-  res.send("Get contact list");
-});
+// app.get("/contacts", validate, (req, res) => {});
 
 // ERROR
 app.get("*", (_req, res) => {
